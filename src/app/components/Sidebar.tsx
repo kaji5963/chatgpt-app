3d78b82afe9b0e7c47d80eb5320b0fffa7fe4ec8
takuya_kajiwara <fd.elt.119@gmail.com>
2023-10-09 21:06:30 +0900

    contextApi実装

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -38,6 +38,10 @@ const Sidebar = () => {
         }));
         setRooms(newRooms);
       });
+
+      return() => {
+        unsubscribe();
+      }
     };
 
     fetchRooms();
---
+++ src/context/AppContext.tsx
@@ -0,0 +1,50 @@
+import { User, onAuthStateChanged } from 'firebase/auth';
+import { ReactNode, createContext, useEffect, useState } from 'react';
+import { auth } from '../../firebase';
+
+const defaultContextData = {
+  user: null,
+  setUser: () => {},
+  userId: null,
+  selectedRoom: null,
+  setSelectedRoom: () => {},
+};
+
+type AppProviderProps = {
+  children: ReactNode;
+};
+
+type AppContextType = {
+  user: User | null,
+  setUser: React.Dispatch<React.SetStateAction<User | null>>,
+  userId: string | null,
+  selectedRoom: string | null,
+  setSelectedRoom: React.Dispatch<React.SetStateAction<string | null>>
+}
+
+const AppContext = createContext<AppContextType>(defaultContextData);
+
+export const AppProvider = ({ children }: AppProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [userId, setUserId] = useState<string | null>(null);
+  const [selectedRoom, setSelectedRoom] = useState<string | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (newUser) => {
+      setUser(newUser);
+      setUserId(newUser? newUser.uid : null);
+    })
+
+    return() => {
+      unsubscribe();
+    }
+  },[]);
+
+  return (
+    <AppContext.Provider
+      value={{ user, setUser, userId, selectedRoom, setSelectedRoom }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
