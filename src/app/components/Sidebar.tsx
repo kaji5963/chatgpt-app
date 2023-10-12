e76e5be114f5bc23dc5802b82f6cdfd72052c6bf
takuya_kajiwara <fd.elt.119@gmail.com>
2023-10-12 22:16:36 +0900

    Room情報取得及びChat情報取得

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -22,7 +22,7 @@ type RoomType = {
 const Sidebar = () => {
   const [rooms, setRooms] = useState<RoomType[]>([]);
 
-  const { user, userId } = useAppContext();
+  const { user, userId, setSelectedRoom, selectedRoom } = useAppContext();
 
   useEffect(() => {
     if (user) {
@@ -52,6 +52,10 @@ const Sidebar = () => {
     }
   }, [userId]);
 
+  const selectRoom = (roomId: string) => {
+    setSelectedRoom(roomId)
+  }
+
   return (
     <div className=" bg-custom-blue h-full overflow-y-auto px-5 flex flex-col">
       <div className="flex-grow">
@@ -65,6 +69,7 @@ const Sidebar = () => {
               <li
                 key={room.id}
                 className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150"
+                onClick={() => selectRoom(room.id)}
               >
                 {room.name}
               </li>
